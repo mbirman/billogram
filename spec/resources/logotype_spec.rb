3f8d263ebc5e9e08fd73550d6dbc10cf77fbcebd
Mark <birmanmark@gmail.com>
2015-09-07 18:15:30 +0300

    Logotype specs

---
+++ spec/resources/logotype_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe Billogram::Logotype do
+  it_behaves_like "a resource with endpoint", "logotype"
+
+  describe ".upload" do
+    let(:params) { {content: '', file_type: 'image/png'} }
+
+    it "is alias of .create" do
+      expect(described_class).to receive(:create).with(params)
+      described_class.upload(params)
+    end
+  end
+end
