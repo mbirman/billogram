a0eaeffd8ad5bac1fda6391d106a83e7a4d80156
Mark <birmanmark@gmail.com>
2015-09-07 06:13:33 +0300

    Add a field to regional sweden

--- lib/billogram/resources/regional_sweden.rb
+++ lib/billogram/resources/regional_sweden.rb
@@ -1,5 +1,6 @@
 module Billogram
   class RegionalSweden < Resource
-    attr_accessor :rotavdrag, :rotavdrag_personal_number, :rotavdrag_description, :reversed_vat
+    attr_accessor :rotavdrag, :rotavdrag_personal_number, :rotavdrag_description,
+      :reversed_vat, :rotavdrag_account
   end
 end
