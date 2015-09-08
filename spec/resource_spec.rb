8e649aecb25f09d427813a36baa8b5e67fc27ab3
Mark <birmanmark@gmail.com>
2015-09-08 11:39:56 +0300

    Fix resource specs

--- spec/resource_spec.rb
+++ spec/resource_spec.rb
@@ -6,13 +6,15 @@ describe Billogram::Resource do
   describe ".build_objects" do
     subject { described_class.build_objects(argument) }
 
+    before { allow_any_instance_of(described_class).to receive("attribute=") }
+
     describe "when hash given" do
       let(:argument) { {attribute: 'test'} }
       it { is_expected.to be_a(Billogram::Resource)}
     end
 
     describe "when array given" do
-      let(:argument) { [{item_no: 1}, {item_no: 2}] }
+      let(:argument) { [{attribute: 'test'}, {attribute: 'test2'}] }
 
       it { is_expected.to be_a(Array) }
 
@@ -49,6 +51,33 @@ describe Billogram::Resource do
 
     it "adds relation to class variable" do
       expect(subject.relations).to include(Billogram::Relation)
+    end
+  end
+
+  describe "#to_hash" do
+    let(:array) { [described_class.new, described_class.new] }
+    let(:resource) { described_class.new }
+    let(:instance_variables) { [:@array, :@resource] }
+
+    subject { described_class.new }
+
+    before do
+      allow(subject).to receive(:instance_variables).and_return(instance_variables)
+      allow(subject).to receive(:instance_variable_get).with(:@array).and_return(array)
+      allow(subject).to receive(:instance_variable_get).with(:@resource).and_return(resource)
+    end
+
+    it "calls #to_hash on a nested resource" do
+      expect(resource).to receive(:to_hash)
+      subject.to_hash
+    end
+
+    it "calls #to_hash on every item in array" do
+      array.each do |item|
+        expect(item).to receive(:to_hash)
+      end
+
+      subject.to_hash
     end
   end
 end
