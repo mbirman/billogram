29d18cefc083e1b0d1b6c34c9eead3cac4c29a63
Mark <birmanmark@gmail.com>
2016-05-27 16:29:32 +0300

    Add autogiro attributes

--- lib/billogram/resources/regional_sweden.rb
+++ lib/billogram/resources/regional_sweden.rb
@@ -1,6 +1,8 @@
 module Billogram
   class RegionalSweden < Resource
     attr_accessor :rotavdrag, :rotavdrag_personal_number, :rotavdrag_description,
-      :reversed_vat, :rotavdrag_account, :rotavdrag_sum
+      :reversed_vat, :rotavdrag_account, :rotavdrag_sum, :autogiro_payment_date,
+      :autogiro_status, :autogiro_full_status, :autogiro_betalarnummer,
+      :autogiro_total_sum
   end
 end
