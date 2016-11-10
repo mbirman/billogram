2919f6cca8bbff18824c7f8dd10a52c8ee84d711
Mark <birmanmark@gmail.com>
2016-11-10 20:57:46 +0300

    Add new attributes to RegionalSweden

--- lib/billogram/resources/regional_sweden.rb
+++ lib/billogram/resources/regional_sweden.rb
@@ -3,6 +3,10 @@ module Billogram
     attr_accessor :rotavdrag, :rotavdrag_personal_number, :rotavdrag_description,
       :reversed_vat, :rotavdrag_account, :rotavdrag_sum, :autogiro_payment_date,
       :autogiro_status, :autogiro_full_status, :autogiro_betalarnummer,
-      :autogiro_total_sum
+      :autogiro_total_sum, :autogiro_payment_date, :autogiro_status,
+      :autogiro_full_status, :efaktura_recipient_bank_code,
+      :efaktura_recipient_bank_id, :efaktura_recipient_type,
+      :efaktura_recipient_bank_name, :autogiro_betalarnummer, :autogiro_total_sum,
+      :efaktura_recipient_identifier, :efaktura_recipient_id_number
   end
 end
