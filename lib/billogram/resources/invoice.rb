2f8f174df1e0c422fd6df7876171c9f2cb009f01
Mark <birmanmark@gmail.com>
2017-06-28 08:11:44 +0300

    Add offers to Invoice

--- lib/billogram/resources/invoice.rb
+++ lib/billogram/resources/invoice.rb
@@ -9,7 +9,7 @@ module Billogram
                   :interest_fee, :currency, :delivery_method, :state, :url, :flags,
                   :remaining_sum, :total_sum, :rounding_value, :automatic_reminders,
                   :reminder_count, :created_at, :attested_at, :updated_at, :detailed_sums,
-                  :attachment
+                  :attachment, :offers
 
     relation :info, :one
     relation :customer, :one
