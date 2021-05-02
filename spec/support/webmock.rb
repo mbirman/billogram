da1ed8df06c7441b917ef1382d2cfb6b462cb621
Mark Birman <birmanmark@gmail.com>
2021-05-02 11:31:41 +0400

    Remove the universal stub

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -3,11 +3,3 @@
 require 'webmock/rspec'
 
 WebMock.disable_net_connect!(allow_localhost: true)
-
-RSpec.configure do |config|
-  config.before do
-    WebMock
-      .stub_request(:any, /#{ENV['BILLOGRAM_BASE_URI']}.*/)
-      .to_return(status: 200, body: { data: {} }.to_json)
-  end
-end
